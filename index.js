d8d7b22ab6d1667b5be9037306dac36c8f4ccd88
Ben Schinn <schinn.ben@gmail.com>
2019-08-22 23:53:05 -0600

    wasm utils

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 WebAssembly.instantiateStreaming(fetch("denice.gc.wasm"))
   .then(wasmModule => {
     const result = wasmModule.instance.exports.add_one(3);
+    console.log(wasmModule.instance.exports)
     const text = document.createTextNode(result);
     document.body.appendChild(text);
   });
--- utils/lib.rs
+++ utils/lib.rs
@@ -2,4 +2,3 @@
 pub extern fn add_one(x: u32) -> u32 {
     x + 1
 }
-
