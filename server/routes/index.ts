5c6be3e0872baa6d3dcdc826a5a93e856ab5d333
Ben Schinn <schinn.ben@gmail.com>
2019-09-01 22:37:54 -0600

    serving static files

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,12 @@
+import express = require('express')
+import path = require('path')
+
 const routesWrapper = (app) => {
+  app.use(express.static(path.resolve(__dirname, '..', '..', 'build')));
+  app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
+  });
+
   app.use('/nyt-recipe', require('./nyt-cooking')) 
 }
 
